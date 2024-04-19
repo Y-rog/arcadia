4d8e91746d7d283f95f94175586033db0f85dd75
Y-rog <fulgueiras.gregory@gmail.com>
2024-04-19 12:51:10 +0200

    heroku test deploy

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'db_name' => 'sf_arcadia',
-    'db_user' => 'admin_sf_arcadia',
-    'db_password' => 'admin@arcadia',
+    'db_name' => 'lkzh4ikd69snza2f',
+    'db_user' => 'hdfqb71qk7iw9krn',
+    'db_password' => 'z6fv4vntk00gkhmn',
     'db_port' => '3306',
-    'db_host' => 'localhost',
+    'db_host' => 'f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
 ];
