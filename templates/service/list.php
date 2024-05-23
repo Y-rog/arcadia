0f5d49b765bfdd84d67abc8a5580a7299041ed33
Y-rog <fulgueiras.gregory@gmail.com>
2024-05-23 11:53:19 +0200

    update service

--- templates/service/list.php
+++ templates/service/list.php
@@ -18,30 +18,31 @@ use App\Security\Security; ?>
         <?php } ?>
         <?php foreach ($services as $service) : ?>
             <div class="py-3">
-                <h6 class="text-secondary"><?= htmlspecialchars($service->getName()); ?></h6>
+                <h6 class="text-secondary"><?= htmlspecialchars($service->getTitle()); ?></h6>
                 <ul class="list-group list-group-flush list-group-horizontal">
                     <li class="list-group-item "><?= htmlspecialchars($service->getDescription()); ?></li>
-                    <div class="d-flex justify-content-around pt-2">
-                        <?php if (Security::isAdmin() || Security::isEmployee()) { ?>
-                            <a href="index.php?controller=service&action=edit&id=<?= $service->getId(); ?>"><button class=" btn btn-outline-secondary">Modifier</button></a>
-                        <?php } ?>
-                        <?php if (Security::isAdmin()) { ?>
-                            <a><button class=" btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteService<?= htmlspecialchars($service->getId()) ?>">Supprimer</button></a>
-                            <div class="modal fade" id="deleteService<?= htmlspecialchars($service->getId()) ?>" tabindex="-1" aria-labelledby="addReviewLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-content text-center">
-                                        <div class="modal-body">
-                                            <p>Etes-vous sûr de vouloir supprimer ce service?</p>
-                                            <form method="POST" class="pb-2">
-                                                <input type="hidden" name="id" value="<?= htmlspecialchars($service->getId()) ?>">
-                                                <input type="submit" class="btn btn-danger" value="Supprimer" name="deleteService">
-                                            </form>
-                                        </div>
+                </ul>
+                <div class="d-flex justify-content-around pt-2">
+                    <?php if (Security::isAdmin() || Security::isEmployee()) { ?>
+                        <a href="index.php?controller=service&action=edit&id=<?= $service->getId(); ?>"><button class=" btn btn-outline-secondary">Modifier</button></a>
+                    <?php } ?>
+                    <?php if (Security::isAdmin()) { ?>
+                        <a><button class=" btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteService<?= htmlspecialchars($service->getId()) ?>">Supprimer</button></a>
+                        <div class="modal fade" id="deleteService<?= htmlspecialchars($service->getId()) ?>" tabindex="-1" aria-labelledby="addReviewLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content text-center">
+                                    <div class="modal-body">
+                                        <p>Etes-vous sûr de vouloir supprimer ce service?</p>
+                                        <form method="POST" class="pb-2">
+                                            <input type="hidden" name="id" value="<?= htmlspecialchars($service->getId()) ?>">
+                                            <input type="submit" class="btn btn-danger" value="Supprimer" name="deleteService">
+                                        </form>
                                     </div>
                                 </div>
                             </div>
-                        <?php } ?>
-                    </div>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         <?php endforeach; ?>
 </main>
